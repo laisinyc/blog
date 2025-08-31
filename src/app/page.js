8c05dfde1623b6df999aa68714e355af976cbf68
kevinlainyc <kevinlainyc@gmail.com>
2025-08-31 18:29:04 -0400

    remove unused code

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <div >
       <main>
         Hello there!!!
       </main>
-      <footer className={styles.footer}>
-        Here is footer
-      </footer>
     </div>
   );
 }

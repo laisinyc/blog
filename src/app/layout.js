17940fa078fb1d9451d2cdad7099c75b5ca43b52
kevinlainyc <kevinlainyc@gmail.com>
2025-09-01 15:33:36 -0400

    remove jwc header and footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,3 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -21,9 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />
         {children}
-        <Footer />
       </body>
     </html>
   );

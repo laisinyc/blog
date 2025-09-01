a31c609f2faa52e525f94f972aff964b67ba79d4
kevinlainyc <kevinlainyc@gmail.com>
2025-09-01 18:49:37 -0400

    add mdx table plugin

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -4,6 +4,7 @@ import { notFound } from 'next/navigation';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import BlogHero from '@/components/BlogHero';
 import { MDXRemote } from 'next-mdx-remote/rsc';
+import remarkGfm from 'remark-gfm';
 
 import styles from './postSlug.module.css';
 import COMPONENT_MAP from '@/helpers/mdx-components'
@@ -23,6 +24,13 @@ async function BlogPost({ params }) {
   // const { frontmatter, content } = await loadBlogPost(postSlug)
   const blogPostData = await loadBlogPost(postSlug);
 
+  const options = {
+    mdxOptions: {
+      remarkPlugins: [remarkGfm],
+      rehypePlugins: [],
+    },
+  };
+
   if (!blogPostData) notFound();
 
   const { frontmatter, content } = blogPostData;
@@ -37,6 +45,7 @@ async function BlogPost({ params }) {
         <MDXRemote
           source={content}
           components={COMPONENT_MAP}
+          options={options}
         />
       </div>
     </article>

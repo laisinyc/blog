ad6baf953224ad58c622df6ab4697af7740dc570
kevinlainyc <kevinlainyc@gmail.com>
2025-09-01 22:00:43 -0400

    update html doc contents

--- src/components/HtmlTags/HtmlTags.js
+++ src/components/HtmlTags/HtmlTags.js
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
-import Card from '@/components/Card';
+// import Card from '@/components/Card';
 import MarkdownCode from '@/components/MarkdownCode';
 import styles from './HtmlTags.module.css';
 
